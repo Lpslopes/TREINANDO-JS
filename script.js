e2b4d36f0198b2b1fcf75afcc3e0b0a54d87f493
Lpslopes <lamartinelps@gmail.com>
2021-06-14 21:09:21 -0300

    Calc_IMC

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
+function imc(){
+    let p1 = parseFloat(document.formImc.peso.value);
+    let alt1 = parseFloat(document.formImc.altura.value);
+    let resultado = p1 /(alt1 * alt1);
+    if (resultado <= 18.5 ){
+        
+    }
+    return document.getElementById("Res").innerHTML = resultado.toFixed(2);
+}
 
 
 
